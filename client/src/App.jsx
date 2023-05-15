5934acef93af6707e12f07650ce72c56db950645
fmikaelian <39884124+fmikaelian@users.noreply.github.com>
2023-05-15 20:22:26 +0200

    add Signature component in the core app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Wallet from "./Wallet";
+import Signature from "./Signature";
 import Transfer from "./Transfer";
 import "./App.scss";
 import { useState } from "react";
@@ -6,6 +7,8 @@ import { useState } from "react";
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
+  const [signatureCompactHex, setSignatureCompactHex] = useState("");
+  const [signatureRecovery, setSignatureRecovery] = useState("");
 
   return (
     <div className="app">
@@ -15,7 +18,13 @@ function App() {
         address={address}
         setAddress={setAddress}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Signature
+        signatureCompactHex={signatureCompactHex}
+        setSignatureCompactHex={setSignatureCompactHex}
+        signatureRecovery={signatureRecovery}
+        setSignatureRecovery={setSignatureRecovery}
+      />
+      <Transfer setBalance={setBalance} signatureCompactHex={signatureCompactHex} signatureRecovery={signatureRecovery} />
     </div>
   );
 }
